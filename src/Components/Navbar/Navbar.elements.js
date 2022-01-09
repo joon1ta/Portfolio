eb3ad414a632c6b5f1f7e54915e56aa712c46f9d
Jonathan Armesto <armestojona@gmail.com>
2022-01-09 10:55:59 -0300

    NavBar Styled Component

---
+++ src/Components/Navbar/Navbar.elements.js
@@ -0,0 +1,144 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    height: 70px;
+`;
+
+export const Wrapper = styled.div`
+    width: 100%;
+    max-width: 1200px;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end; 
+`;
+
+
+export const Menu = styled.ul`
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+
+    @media screen and (max-width: 960px) {
+        position: absolute;
+        top: 70px;
+        left: ${({open}) => open ? '0' : '-100%' };
+        width: 100%;
+        height: 90vh;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        transition: 0.5s all ease;
+    }
+`;
+
+export const MenuItem = styled.li`
+    height: 100%;
+
+    @media screen and (max-width: 960px) {
+        width: 100%;
+        height: 70px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+`;
+
+export const MenuItemLink = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding: 0.5rem 2.5rem;
+    color: var(--eggShell);
+    font-size: 1rem;
+    cursor: pointer;
+    transition: 0.5s all ease;
+
+    button {
+        background: transparent;
+        border-radius: 5px;
+        padding: .5rem;
+        font-size: .7rem;
+        color: var(--coral);
+        display: block;
+        border: 1px solid var(--coral);
+    }
+
+    div{
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+
+        svg {
+            display: none;
+            fill: var(--eggShell);
+            
+        }
+    }
+
+    &:hover {
+        color: var(--coral);
+        transition: 0.5s all ease;
+    }
+
+    @media screen and (max-width: 960px) {
+        width: 100%;
+        div {
+            width: 30%;
+            justify-content: left;
+
+            svg {
+                display: flex;
+            }
+        }
+
+    }
+    @media screen and (max-width: 880px) {
+    div {
+      width: 40%;
+      justify-content: left;
+      svg {
+        display: flex;
+      }
+    }
+  }
+  @media screen and (max-width: 500px) {
+    div {
+      width: 75%;
+      justify-content: left;
+      svg {
+        display: flex;
+      }
+    }
+  }
+  @media screen and (max-width: 260px) {
+    div {
+      width: 100%;
+      justify-content: left;
+      svg {
+        display: flex;
+      }
+    }
+  }
+`;
+
+
+export const MobileIcon = styled.div `
+    display: none;
+
+    @media screen and (max-width: 960px){
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+
+        svg{
+           fill: var(--eggShell);
+           margin-right: 0.5rem;
+        }
+    }
+`
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
+import {
+    Container,
+    Wrapper,
+    Menu,
+    MenuItem,
+    MenuItemLink,
+    MobileIcon,
+} from "./Navbar.elements";
+import { FaAlignRight, FaAngleRight, FaDownload, FaTimes } from "react-icons/fa";
+const Navbar = () => {
+
+    const [ShowMenu, SetShowMenu] = useState(false)
+
+
+
+
+    return (
+        <Container>
+            <Wrapper>
+                <IconContext.Provider value={{ style: { fontSize: "1.5em", margin: ".5em" } }}>
+
+
+                    <MobileIcon onClick={() => SetShowMenu(!ShowMenu)}>
+                        {
+                            ShowMenu ? <FaTimes /> : <FaAlignRight />
+                        }
+
+                    </MobileIcon>
+
+                    <Menu open={ShowMenu}>
+                        <MenuItem>
+                            <MenuItemLink onClick={() => SetShowMenu(!ShowMenu)}>
+                                <div>
+                                    <FaAngleRight />
+                                    About
+                                </div>
+
+                            </MenuItemLink>
+                        </MenuItem>
+                        <MenuItem>
+
+                            <MenuItemLink onClick={() => SetShowMenu(!ShowMenu)}>
+                                <div>
+                                    <FaAngleRight />
+                                    Experience
+                                </div>
+
+                            </MenuItemLink>
+                        </MenuItem>
+
+                        <MenuItem>
+                            <MenuItemLink onClick={() => SetShowMenu(!ShowMenu)}>
+                                <div>
+                                    <FaAngleRight />
+                                    Work
+                                </div>
+
+                            </MenuItemLink>
+                        </MenuItem>
+
+                        <MenuItem>
+                            <MenuItemLink onClick={() => SetShowMenu(!ShowMenu)}>
+                                <div>
+                                    <FaAngleRight />
+                                    Contact
+                                </div>
+
+                            </MenuItemLink>
+                        </MenuItem>
+
+                        <MenuItem>
+                            <MenuItemLink onClick={() => SetShowMenu(!ShowMenu)}>
+                                <div>
+                                    <FaDownload />
+                                    <button>Resume</button>
+                                </div>
+                            </MenuItemLink>
+                        </MenuItem>
+                    </Menu>
+                </IconContext.Provider>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Navbar;
